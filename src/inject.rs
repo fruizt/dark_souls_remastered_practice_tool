5cd243f4fe7e07316f891a7001d0170fc036ca84
Fabian Ruiz <fabianruizt4@gmail.com>
2024-09-16 23:46:23 -0500

    INIT: able to get the game handle II

---
+++ src/inject.rs
@@ -0,0 +1,50 @@
+use std::mem;
+
+use windows::core::{Error, Result, HRESULT, HSTRING};
+use windows::Win32::Foundation::{CloseHandle, BOOL, HANDLE};
+use windows::Win32::System::Diagnostics::ToolHelp::{
+    CreateToolhelp32Snapshot, Process32FirstW, Process32NextW, PROCESSENTRY32W, TH32CS_SNAPPROCESS,
+};
+use windows::Win32::System::Threading::{OpenProcess, PROCESS_ALL_ACCESS};
+
+// Look for the game
+pub fn get_process_by_name(name: &str) -> Result<HANDLE> {
+    unsafe { get_process_by_name64(name) }
+}
+
+unsafe fn get_process_by_name64(name_str: &str) -> Result<HANDLE> {
+    let name = HSTRING::from(name_str);
+
+    let snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0)?;
+    let mut process_entry32 = PROCESSENTRY32W {
+        dwSize: mem::size_of::<PROCESSENTRY32W>() as u32,
+        ..Default::default()
+    };
+
+    if Process32FirstW(snapshot, &mut process_entry32).is_err() {
+        CloseHandle(snapshot)?;
+        return Err(Error::from_win32());
+    }
+
+    let pid = loop {
+        let zero_idx = process_entry32
+            .szExeFile
+            .iter()
+            .position(|&x| x == 0)
+            .unwrap_or(process_entry32.szExeFile.len());
+        let process_name = HSTRING::from_wide(&process_entry32.szExeFile[..zero_idx])?;
+
+        if name == process_name {
+            break Ok(process_entry32.th32ProcessID);
+        }
+
+        if Process32NextW(snapshot, &mut process_entry32).is_err() {
+            CloseHandle(snapshot)?;
+            break Err(Error::from_hresult(HRESULT(-1)));
+        }
+    }?;
+
+    CloseHandle(snapshot)?;
+
+    OpenProcess(PROCESS_ALL_ACCESS, BOOL(0), pid)
+}
